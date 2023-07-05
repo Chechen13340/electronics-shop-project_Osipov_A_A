2fb3e59dfe383e193335b3581db44e981958ac99
Artem Osipov <tema.osipov@mail.ru>
2023-07-05 17:19:01 +0300

    edit PEP8

--- tests/test_item.py
+++ tests/test_item.py
@@ -5,12 +5,15 @@ from src.item import Item
 
 
 @pytest.fixture
+
+
 def properties_Item():
     return Item("Смартфон", 10000, 20)
 
 
 def test_calculate_total_price(properties_Item):
     assert properties_Item.calculate_total_price() == 200000
+
 
 def test_apply_discount(properties_Item):
     Item.pay_rate = 0.8
