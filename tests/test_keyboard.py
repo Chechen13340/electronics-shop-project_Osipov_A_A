e133adb1bb511bdfcf8d48be0ded1f058348bc72
Artem Osipov <tema.osipov@mail.ru>
2023-07-21 22:40:37 +0300

    created file test_keyboard. Written test for class Keyboard

---
+++ tests/test_keyboard.py
@@ -0,0 +1,40 @@
+import pytest
+
+from src.keyboard import Keyboard
+
+
+@pytest.fixture
+def properties_keyboard():
+    return Keyboard('Dark Project KD87A', 9600, 5)
+
+
+def test_init_(properties_keyboard):
+    assert properties_keyboard.name == 'Dark Project KD87A'
+    assert properties_keyboard.price == 9600
+    assert properties_keyboard.quantity == 5
+
+
+def test_str_(properties_keyboard):
+    assert str(properties_keyboard) == 'Dark Project KD87A'
+
+
+def test_language(properties_keyboard):
+    assert properties_keyboard.language == 'EN'
+
+
+def test_change_lang(properties_keyboard):
+    properties_keyboard.change_lang()
+    assert properties_keyboard.language == 'RU'
+    properties_keyboard.change_lang().change_lang()
+    assert properties_keyboard.language == 'RU'
+    properties_keyboard.change_lang().change_lang().change_lang()
+    assert properties_keyboard.language == 'EN'
+
+#
+# def test_number_of_sim_setter(properties_phone):
+#     properties_phone.number_of_sim = 4
+#     assert properties_phone.number_of_sim == 4
+#
+# def test_number_of_sim_setter_error(properties_phone):
+#     with pytest.raises(ValueError):
+#         properties_phone.number_of_sim = 0
